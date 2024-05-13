330d9c3ac6cdd712cb94c18b65362823fbc28a69
ChrisMaia <anny.lmaia@gmail.com>
2024-05-13 13:10:20 -0300

    primeiro git

---
+++ resources/views/users.blade.php
@@ -0,0 +1,17 @@
+@extends('master')
+
+@section('content')
+
+<h2>Users</h2>
+
+<a href="{{route('users.create')}}">Criar</a>
+
+<hr>
+
+<ul>
+    @foreach($users as $user)
+        <li>{{$user->name}} | <a href="{{route('users.edit', ['user' => $user->id])}}">Editar</a> | <a href="{{route('users.destroy', ['user' => $user->id])}}">Deletar</a></li>
+    @endforeach
+</ul>
+
+@endsection
